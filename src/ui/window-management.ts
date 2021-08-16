5d39d65505ba0da4e25710d02580fe15ce330560
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2021-08-16 16:57:59 +0000

    Window Management v0

---
+++ src/ui/window-management.ts
@@ -0,0 +1,60 @@
+import browser from "webextension-polyfill"
+
+
+async function createPopUpWindow(url : string, callback? : Function) {
+  let window = await browser.windows.create({
+    url: url,
+    type: "popup",
+    width: 360,
+    left: 1080,
+    focused: true,
+    top: 0
+  })
+  
+  // popUpWindowId = window.id
+  // popUpWindowTabId = window.tabs[0].id
+
+  // Wait until page is loaded to inject the content script, or else a
+  // "cannot access contents of url """ error will be raised:
+  if (callback) callback(window)
+  return
+}
+
+async function getPopUpWindow(popUpWindowId: number) {
+  if (popUpWindowId == null) return null
+
+  try { return await browser.windows.get(popUpWindowId) } catch (e) {}
+
+  return null
+}
+
+async function focusPopUpWindow(popUpWindowId: number) {
+  return await browser.windows.update(popUpWindowId, {
+    drawAttention: true,
+    focused: true
+  })
+}
+
+export function getPopUpTab(popUpWindowTabId: number) {
+  if (typeof popUpWindowTabId === 'undefined' || !popUpWindowTabId) return
+
+  return browser.tabs.get(popUpWindowTabId)
+}
+
+export async function showPopUpWindow(url: string, popUpWindowId?: number, callback? : Function) {
+  if (popUpWindowId) {
+    let window = await getPopUpWindow(popUpWindowId)
+    if (window) return await focusPopUpWindow(window)
+  }
+
+  return await createPopUpWindow(url)
+}
+
+export default { showPopUpWindow, getPopUpTab }
+
+
+function myFunction(window) {
+  // guardo el ID
+}
+
+showPopUpWindow('https://google.com', null, myFunction)
