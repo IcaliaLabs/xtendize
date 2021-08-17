48b3515e8e3a61fddcdcf68cb2d8c2f81af26261
Roberto Quintanilla <vov@icalialabs.com>
2021-08-17 17:17:36 +0000

    WIP

--- src/ui/window-management.ts
+++ src/ui/window-management.ts
@@ -1,44 +1,42 @@
-import browser from "webextension-polyfill"
+///<reference types="chrome"/>
 
-import Tab = browser.Tabs.Tab
-import WindowCreateData = browser.Windows.CreateCreateDataType
-import Window = browser.Windows.Window
+import Tab = chrome.tabs.Tab
+import WindowCreateData = chrome.windows.CreateData
+import Window = chrome.windows.Window
 
-async function createPopUpWindow(createData: WindowCreateData) : Promise<Window> {
+function createPopUpWindow(createData: WindowCreateData) : Promise<Window> {
   createData.type = "popup"
   createData.focused = true
-
-  let window = browser.windows.create(createData)
-
-  // TODO: Store the window id on chrome.storage
-  console.log("window:", window)
-
-  return window
+  debugger
+  console.log("createData:", createData)
+  return chrome.windows.create(createData)
 }
 
 async function getPopUpWindow() : Promise<Window> {
   let popUpWindowId = -1 // TODO: Get popUpWindowId from chrome.storage
-  return await browser.windows.get(popUpWindowId)
+  return await chrome.windows.get(popUpWindowId)
 }
 
 function focusPopUpWindow(windowId: number) : Promise<Window> {
-  return browser.windows.update(windowId, {
+  return chrome.windows.update(windowId, {
     drawAttention: true,
     focused: true
   })
 }
 
 export async function getPopUpTab(popUpWindowTabId: number) : Promise<Tab> {
-  return await browser.tabs.get(popUpWindowTabId)
+  return await chrome.tabs.get(popUpWindowTabId)
 }
 
-export async function showPopUpWindow(createData: WindowCreateData) : Promise<Window> {
-  try {
-    let window = await getPopUpWindow()
-    if (window.id) return focusPopUpWindow(window.id)
-  } catch (e) {}
+export async function showPopUpWindow(createData: WindowCreateData) : Promise<Window | null> {
+  // try {
+  //   let window = await getPopUpWindow()
+  //   if (window.id) return focusPopUpWindow(window.id)
+  // } catch (e) {}
 
-  return await createPopUpWindow(createData)
+  console.log("showPopUpWindow: createData:", createData)
+  // return await createPopUpWindow(createData)
+  return null
 }
 
 export default { showPopUpWindow, getPopUpTab }
